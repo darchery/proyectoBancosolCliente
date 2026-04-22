28f06fccd783d9922b884249e1229bca30e0a22e
darchery <anestardiaz@gmail.com>
2026-04-22 10:50:48 +0200

    Refactor class button logout and remove id from logout button

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,9 @@
-const btnLogOut = document.querySelectorAll(".logout-btn");
+// Manejo del botón de cerrar sesión por clase
+const btnLogOut = document.querySelector(".boton-logout");
 
 if (btnLogOut) {
     btnLogOut.addEventListener("click", () => {
+        // Redirigir a la página de inicio (subiendo un nivel desde GestionUsuarios)
         window.location.href = "index_home.html";
     });
 }
