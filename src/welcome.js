bf5d5fdf19dd0d803df3e3f0c3911279fcc800eb
darchery <anestardiaz@gmail.com>
2026-04-22 10:52:30 +0200

    Update welcome.js
    Refactor window location href to ./index_home.html

--- src/welcome.js
+++ src/welcome.js
@@ -4,6 +4,6 @@ const btnLogOut = document.querySelector(".boton-logout");
 if (btnLogOut) {
     btnLogOut.addEventListener("click", () => {
         // Redirigir a la página de inicio (subiendo un nivel desde GestionUsuarios)
-        window.location.href = "index_home.html";
+        window.location.href = "../index_home.html";
     });
 }
