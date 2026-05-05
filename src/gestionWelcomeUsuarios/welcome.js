b62cc56a65256461f11c531013dab9d1cb27c64a
Sergio Aldana <sergioaldanagonzalez@gmail.com>
2026-05-05 13:59:47 +0200

    reorganizado todo el proyecto

--- src/gestionWelcomeUsuarios/welcome.js
+++
@@ -1,9 +0,0 @@
-// Manejo del botón de cerrar sesión por clase
-const btnLogOut = document.querySelector(".boton-logout");
-
-if (btnLogOut) {
-    btnLogOut.addEventListener("click", () => {
-        // Redirigir a la página de inicio (subiendo un nivel desde GestionUsuarios)
-        window.location.href = "../index_home.html";
-    });
-}
