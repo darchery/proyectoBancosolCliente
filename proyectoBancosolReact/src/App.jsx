ea48c24350be33e7b6c2a6fd50c5e9a812797149
BlinckyP <mariinaruiizuni@gmail.com>
2026-05-27 09:52:45 +0200

    version incial colaboradores react

--- proyectoBancosolReact/src/App.jsx
+++ proyectoBancosolReact/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import GestionColaboradores from './pages/gestion/colaboradores/gestion_colaboradores'
+
 
 function App() {
 
   return (
-    <>
-      
-            
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/gestion/colaboradores" element={<GestionColaboradores />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
